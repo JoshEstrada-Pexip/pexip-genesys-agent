17f3e75a590a5d037bf1f4b364bc13e4efbc4a16
joshestrada <88341392+joshestrada@users.noreply.github.com>
2025-03-23 01:05:36 -0700

    Update config.js

--- agent-app/scripts/config.js
+++ agent-app/scripts/config.js
@@ -7,7 +7,7 @@ export default {
 
     // Publicly accessible location where the admin-app files are hosted.
     // This is different than the Pexip conference node value below.
-    prodUri:  'https://github.com/joshestrada/pexip-genesys-agent/blob/main/agent-app/index.html',
+    prodUri:  'https://github.com/joshestrada/pexip-genesys-agent/',
 
     // Id for the video DOM element. Only change this if you customize index.html.
     videoElementId: "pexip-video-container",
